a3dd86966e7200517f802e0cdb77547a7190c3a0
Patrick Valdivia <patrick.v.valdivia@gmail.com>
2024-06-20 15:46:20 +0200

    distMiddlewareAdded

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ const requestLogger = (request, response, next) => {
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 app.use(requestLogger)
 
 const unknownEndpoint = (request, response) => {
