b1d3d5be8594de93a8190bb80994be5d43660954
Patrick <patrick.v.valdivia@gmail.com>
2024-06-24 13:48:24 +0200

    ControlenMongo

--- index.js
+++ index.js
@@ -37,12 +37,6 @@ app.use(requestLogger)
 
 app.use(express.static('dist'))
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-
-
 app.get('/',(req,res)=>{
   res.send('<h1>Hello World¡</h1>')
 })  
@@ -53,25 +47,26 @@ app.get('/api/notes',(req,res)=>{
   })
 })
 
-app.get('/api/notes/:id',(req,res)=>{
+app.get('/api/notes/:id',(req,res, next)=>{
   Note.findById(req.params.id).then(note =>{
-    res.json(note)
-  })
+    if(note){
+      res.json(note)
+    }else{
+      res.status(404).end()
+    }
+  }).catch(error =>next(error))
 })
 
 app.delete('/api/notes/:id',(req,res) =>{
-  const id = Number(req.params.id)
-  notes = notes.filter(note => note.id !== id)
-  res.send('Note deleted')
-  res.status(204).end()
+  Note.findByIdAndDelete(req.params.id)
+    .then(result =>{
+      res.status(204).end()
+    }).catch(error => next(error))
 })
 
-app.post('/api/notes',(req,res) =>{
+app.post('/api/notes',(req,res, next) =>{
 
   const body = req.body
-  if(body.content === undefined){
-    return res.status(404).json({error:'content missing'})
-  }
   const note = new Note ({
     content: body.content,
     important: body.important||false
@@ -79,11 +74,39 @@ app.post('/api/notes',(req,res) =>{
 
   note.save().then(savedNote =>{
     res.json(savedNote)
-  })
+  }).catch(error => next(error))
 })
 
-app.use(unknownEndpoint)
+app.put('/api/notes/:id',(req,res,next)=>{
+  const body = req.body
+  const note = {
+    content: body.content,
+    important: body.important
+  }
+  Note.findByIdAndUpdate(req.params.id, note,{new:true, runValidators: true, context:'query'})
+    .then(updatedNote =>{
+      res.json(updatedNote)
+    }).catch(error => next(error))
+})
 
+//Error a través del middleware para endpoint desconocido
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint) 
+
+//Error a través del middleware para solicitudes que den error
+const errorHandler = (error, req, res, next)=>{
+  console.log(error.message)
+  if(error.name === 'CastError'){
+    return res.status(400).send({error:'Wrong Id'})
+  }else if(error.name === 'ValidationError'){
+    return res.status(400).json({error:error.message})
+  }
+  next(error)
+}
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
