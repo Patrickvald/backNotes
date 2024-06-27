47a7f92d4ae960df9899893342c3bc9041383f21
Patrick <patrick.v.valdivia@gmail.com>
2024-06-27 14:57:41 +0200

    ad asynxawait y evitar trycatch

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,40 +1,41 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
   
-  notesRouter.get('/',(req,res)=>{
-    Note.find({}).then(notes =>{
+  notesRouter.get('/', async(req,res)=>{
+    const notes = await Note.find({})
       res.json(notes)
-    })
   })
   
-  notesRouter.get('/:id',(req,res, next)=>{
-    Note.findById(req.params.id).then(note =>{
+  notesRouter.get('/:id', async(req,res, next)=>{
+      const note = await Note.findById(req.params.id)
       if(note){
         res.json(note)
       }else{
         res.status(404).end()
       }
-    }).catch(error =>next(error))
+ /*    Note.findById(req.params.id).then(note =>{
+      if(note){
+        res.json(note)
+      }else{
+        res.status(404).end()
+      }
+    }).catch(error =>next(error)) */
   })
   
-  notesRouter.delete('/:id',(req,res,next) =>{
-    Note.findByIdAndDelete(req.params.id)
-      .then(() =>{
-        res.status(204).end()
-      }).catch(error => next(error))
+  notesRouter.delete('/:id', async(req,res,next) =>{
+    await Note.findByIdAndDelete(req.params.id)
+    res.status(204).end()
   })
   
-  notesRouter.post('/',(req,res, next) =>{
+  notesRouter.post('/', async(req,res, next) =>{
   
     const body = req.body
     const note = new Note ({
       content: body.content,
       important: body.important||false
     })
-  
-    note.save().then(savedNote =>{
-      res.json(savedNote)
-    }).catch(error => next(error))
+      const savedNote = await note.save()
+      res.status(201).json(savedNote)
   })
   
   notesRouter.put('/:id',(req,res,next)=>{
